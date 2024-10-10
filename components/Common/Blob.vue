88a4f30178a33ba70ca64a9844e78968c1f14827
gkarapiperidis <gkarapiperidis@gmail.com>
2024-10-10 07:28:08 +0000

    Initial commit

---
+++ components/Common/Blob.vue
@@ -0,0 +1,4 @@
+<template>
+    <div class="bg-primary w-14 h-14" />
+</template>
+<script setup lang="ts"></script>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [],
+    theme: {
+        colors: {
+            primary: '#EA6623'
+        }
+    },
+    plugins: []
+}
